785045d6291f548d6ff428274eb9eade0ea805d0
narayansingh <narayan.dhillon@gmail.com>
2016-10-29 17:04:25 +0100

    test

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -6,7 +6,7 @@ package com.example;
 public class HelloWorld {
 
     public String sayHello() {
-        return "Hello World";
+        return "Hello World3";
     }
 
     public static void main(String... args) {
